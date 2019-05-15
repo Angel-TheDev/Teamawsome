38bbac6137bc166053c94ebb8a31327bd4eded7d
Bridget Klingler <bridget.klingler@gmail.com>
2019-05-15 11:24:28 -0400

    B changes

--- Teamawsome/Program.cs
+++ Teamawsome/Program.cs
@@ -7,6 +7,10 @@ namespace Teamawsome
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("It's nice to meet you!");
+            Console.WriteLine("Thank you for bringing the sunshine.");
+            Console.ReadLine();
+
         }
     }
 }
